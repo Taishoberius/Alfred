e700b4a8b9c1e342b902a73e1f41fc652e10cfd1
David <dlinhares@reseau-ges.fr>
2019-12-01 18:51:07 +0100

    Add handling off get et post values for openhab

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,42 +10,37 @@ const app = dialogflow({
 
 const openhab = Swagger('http://demo.openhab.org:8080/rest/swagger.json');
 
-openhab.then((client: any) => client
-    .apis
-    .items
-    .getItemData({itemname: "FF_Office_Light"})
-    .then((data: any) => console.log(data)))
 
+app.intent('smart_home_action', (conv) => {
+    const action = conv.parameters.smart_action + "";
+    const target = conv.parameters.smart_object + "";
 
-app.intent('smarthome.device.switch.on', (conv) => {
-    switch (conv.action) {
-        case 'smarthome.device.switch.on':
-            const params = conv.parameters;
-            const room = params.room;
-            const device = params.device;
-            if (room === 'office' && device == 'table lamp') {
-                openhab.then((client: any) => {
-                    client.apis.items.postItemCommand({
-                        itemname: 'FF_Office_Light',
-                        body: 'OFF'
-                    });
-                    conv.close('Done');
-                });
-            }
-            break;
-        default:
-            conv.ask("I can't do that action yet.")
-            break;
+    if (action == "") {
+        openhab.then((client: any) => {
+            client.apis.items.getItemData({
+                itemname: target
+            })
+            .then((res: any) => {
+                console.log(res.obj.state);
+            });
+            conv.close('Done');
+        });
+        return;
     }
-
-    // conv.ask(new SimpleResponse({
-    //     speech: `You asked me to : ${conv.query}`,
-    //     text: `Your parametes are : ${JSON.stringify(conv.parameters)}`
-    // }))
+    
+    openhab.then((client: any) => {
+        console.log("sending " + action + " for " +  target + " to openhab");
+        client.apis.items.postItemCommand({
+            itemname: target,
+            body: action
+        });
+        conv.close('Done');
+    });
 });
+
 
 const expressApp = express().use(BodyParser.json())
 expressApp.post('/intents', app);
-expressApp.listen(6666);
+expressApp.listen(8080);
 
 //exports.intents = functions.https.onRequest(app);àç)
